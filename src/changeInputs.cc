e1d53c5cca41d52a145e8b9a0aa5d4c27689d0bd
og113 <og113@imperial.ac.uk>
2015-10-22 13:27:26 +0100

    fixed changeParameters

--- src/changeInputs.cc
+++ src/changeInputs.cc
@@ -52,9 +52,14 @@ if (argc % 2 && argc>1) {
 		if (id[0]=='-') id = id.substr(1);
 		if (id.compare("Nl")==0) p.Nl = stringToNumber<uint>(argv[2*j+2]);
 		else if (id.compare("Ng")==0) p.Ng = stringToNumber<uint>(argv[2*j+2]);
-		else if (id.compare("Nms")==0) p.Nms = stringToNumber<uint>(argv[2*j+2]);
+		else if (id.compare("Nig")==0) p.Nig = stringToNumber<uint>(argv[2*j+2]);
+		else if (id.compare("Nsw")==0) p.Nsw = stringToNumber<uint>(argv[2*j+2]);
+		else if (id.compare("Npsw")==0) p.Npsw = stringToNumber<uint>(argv[2*j+2]);
 		else if (id.compare("K")==0) p.K = stringToNumber<uint>(argv[2*j+2]);
 		else if (id.compare("G")==0) p.G = stringToNumber<number>(argv[2*j+2]);
+		else if (id.compare("B")==0) p.B = stringToNumber<number>(argv[2*j+2]);
+		else if (id.compare("T")==0) p.T = stringToNumber<number>(argv[2*j+2]);
+		else if (id.compare("Epsi")==0) p.Epsi = stringToNumber<number>(argv[2*j+2]);
 		else if (id.compare("f")==0 || id.compare("fi")==0 || id.compare("fileIn")==0);
 		else if (id.compare("fo")==0 || id.compare("fileOut")==0);
 		else {
