d0bc3d15b0ca684aff5df18081f911b79fed6aa3
Oli Gould <og113@imperial.ac.uk>
2016-03-30 21:51:25 +0100

    removed configPlots

--- configPlots.mk
+++
@@ -1,6 +0,0 @@
-# configuration file for Makeplots
-
-DDIR			= db/nr/plots
-PDIR			= pics/nr/db
-PLOTTER			= tools/plotSimple.sh
-EXE				= bash $(PLOTTER)
--- tools/deltaNr.sh
+++ tools/deltaNr.sh
@@ -5,16 +5,10 @@ i='results/nr/nr.csv'
 m='db/nr/nr.csv'
 o='db/nr/delta.csv'
 
-ls db/nr
-
 comm -23 <(sort $i) <(sort $m) > $o
-
-ls db/nr
 
 i=${i##*/}
 
-ls db/nr
-
 sed -i "s/$/,$i/" $o
 
-ls db/nr
+# to append db/nr/delta.csv to the end of db/nr/nr.csv, type "cat db/nr/delta.csv >> db/nr/nr.csv"
