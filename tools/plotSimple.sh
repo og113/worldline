b6c849742cdaf22d0a6898cea82af736e800ffd1
Oli Gould <og113@imperial.ac.uk>
2016-03-31 10:23:27 +0100

    improved plotSimple.sh and plotLoops.sh to take folders as arguments, without subfolders

--- tools/plotSimple.sh
+++ tools/plotSimple.sh
@@ -2,10 +2,10 @@
 # program to plot using gp/simple.gp
 
 gpFile='gp/simple.gp'
-single=false
+single=true
 oflag=false
 
-# checking of outFile required and getting filename if so
+# checking if outFile required and getting filename if so
 options=':o:'
 OPTIND=1
 while getopts $options option
@@ -18,13 +18,31 @@ do
 done
 shift $((OPTIND-1))
 
-# checking if multiple files are being plotted
+# checking that input files were specified
 if [ -z "$1" ]
-then
+	then
 	echo "must supply input file";
-elif [ -z "$2" ]
+fi
+
+mTemp=""
+for f in "$@";
+do 
+	if [ -e "$f" ] && [ -f "$f" ];
+	then
+		mTemp+="$f ";
+	elif [ -e "$f" ] && [ -d "$f" ]; # only looks one directory deep, and fails if there are subdirectories
+	then
+		mTemp+=$(ls $f/*.dat | tr '\n' ' ')
+		mTemp+=" "
+	fi
+done
+m=${mTemp::-1}
+
+# checking if multiple files are being plotted
+mwords=$(echo $m | wc -w)
+if [ "$mwords" -gt 1 ]
 then
-	single=true;
+	single=false;
 fi
 
 # plotting
@@ -32,15 +50,15 @@ if $single
 then
 	if $oflag
 	then
-		gnuplot -e "inFile='$*'; outFile='$o'" $gpFile;
+		gnuplot -e "inFile='$m'; outFile='$o'" $gpFile;
 	else
-		gnuplot -e "inFile='$*'" $gpFile;
+		gnuplot -e "inFile='$m'" $gpFile;
 	fi
 else
 	if $oflag
 	then
-		gnuplot -e "inFiles='$*'; outFile='$o'" $gpFile;
+		gnuplot -e "inFiles='$m'; outFile='$o'" $gpFile;
 	else
-		gnuplot -e "inFiles='$*'" $gpFile;
+		gnuplot -e "inFiles='$m'" $gpFile;
 	fi
 fi
