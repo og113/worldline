cf164697ccdb26307d5348077b16035a91044903
Oli Gould <og113@imperial.ac.uk>
2016-04-01 17:15:10 +0100

    3dPotentialExtrema written, not tested

--- src/3dPotentialExtrema.cc
+++ src/3dPotentialExtrema.cc
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "simple.h"
 #include "gsl_extras.h"
 #include "3dpotential.h"
@@ -14,8 +15,9 @@
 -------------------------------------------------------------------------------------------------------------------------
 CONTENTS
 	1 - loading inputs and argv
-	2 - finding minima and maxima
-	3 - printing results
+	2 - setting up
+	3 - parameter loop, finding minima and maxima
+	4 - printing results
 -------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------*/
 
@@ -45,24 +47,37 @@ if (argc % 2 && argc>1) {
 	}
 }
 
-if (verbose)
-	cout << "using inputs file " << inputsFile << endl;
-
 // loading inputs
 ParametersRange pr;
 pr.load(inputsFile);
-Parameters p = pr.Min;
-params_for_V params;
-params.setFromParameters(p);
+
+if (verbose) {
+	cout << "3dPotentialExtrema:" << endl << endl;
+	cout << "using inputs file " << inputsFile << ":" << endl;
+}
 
 /*-------------------------------------------------------------------------------------------------------------------------
-	2 - finding minima and maxima
+	2 - setting up
 -------------------------------------------------------------------------------------------------------------------------*/
+
+// parameters
+params_for_V params;
+params.setFromParameters(pr.Min);
+double a_min = (pr.Min).Epsi;
+double a_max = (pr.Max).Epsi;
+double tol = (pr.Min).Lambda;
+uint Npl = pr.totalSteps();
+uint minRuns = 2;
+if (verbose) {
+	cout << "a_min = " << a_min << ", a_max = " << a_max << endl;
+	cout << "bisecting a maximum of " << Npl << " times" << endl;
+	cout << "aiming for a tolerance of " << tol << endl;
+}
 
 // initial guesses and ranges
 double max_guess = sqrt(params.kappa/4.0/PI);
-double max_l = max_guess - 2.0*params.a;
-double max_r = max_guess + 2.0*params.a;
+double max_l = max_guess - params.a;
+double max_r = max_guess + params.a;
 double min_guess = params.a;
 double min_l = 0.0;
 double min_r = max_l;
@@ -72,22 +87,89 @@ gsl_function dV_gsl;
 dV_gsl.function = &dV;
 dV_gsl.params = &params;
 
-// finding extrema
-double max = brentRootFinder(&dV_gsl,max_guess,max_l,max_r);
-double min = brentRootFinder(&dV_gsl,min_guess,min_l,min_r);
+/*-------------------------------------------------------------------------------------------------------------------------
+	2 - finding minima and maxima
+-------------------------------------------------------------------------------------------------------------------------*/
 
-// finding value of V at min and max
-double Vmax = V(max,&params);
-double Vmin = V(min,&params);
+// objects to hold data
+vector<double> max_vec(0), min_vec(0), Vmax_vec(0), Vmin_vec(0);
+double Vmin_l = 0.0, Vmin_r = 0.0, test = 1.0;
+
+if (verbose) {
+	printf("\n");
+	printf("%6s%6s%24s%24s%24s%24s%24s\n","run","kappa","a","max","min","Vmax","Vmin");
+}
+
+uint run = 0;
+
+while((test>tol || run<minRuns) && run<Npl) {
+	
+	// stepping parameters
+	if (run==0) {
+		params.a = a_min;
+	}
+	else if (run==1) {
+		params.a = a_max;
+	}
+	else if (run>1) {
+		params.a = 0.5*(a_min + a_max);
+	}
+	
+	max_l = max_guess - params.a;
+	max_r = max_guess + params.a;
+	min_guess = params.a;
+	min_l = 0.0;
+	min_r = max_l;	
+	
+	// finding extrema
+	max_vec.push_back(brentRootFinder(&dV_gsl,max_guess,max_l,max_r));
+	min_vec.push_back(brentRootFinder(&dV_gsl,min_guess,min_l,min_r));
+
+	// finding value of V at min and max
+	Vmax_vec.push_back(V(max_vec[run],&params));
+	Vmin_vec.push_back(V(min_vec[run],&params));
+	
+	// test
+	test = abs(Vmin_vec[run]);
+	
+	if (run==0) {
+		Vmin_l = Vmin_vec[0];
+	}
+	else if (run==1) {
+		Vmin_r = Vmin_vec[1];
+		// checking to see if change of sign over range of 'a'
+		if (Vmin_l*Vmin_r>0.0) {
+			cerr << "error: Vmin_l*Vmin_r>0.0)" << endl;
+			cerr << "change range of 'a' to search" << endl;
+			break;
+			return 1;
+		}
+	}
+	else if (run>1) {
+		// doing bifurcation
+		if (Vmin_vec[run]*Vmin_l<0.0) {
+			a_max = params.a;
+		}
+		else {
+			a_min = params.a;
+		}
+	}
+	
+	if (verbose)
+		printf("%6i%6.2g%24.16g%24.16g%24.16g%24.16g%24.16g\n",run,params.kappa,params.a,\
+			max_vec[run],min_vec[run],Vmax_vec[run],Vmin_vec[run]);
+		
+	run++;
+}
+
+if (verbose)
+	printf("\n");
 
 /*-------------------------------------------------------------------------------------------------------------------------
 	3 - printing results	
 -------------------------------------------------------------------------------------------------------------------------*/
 
-printf("\n");
-printf("%20s%20s%20s%20s%20s%20s\n","kappa","a","max","min","Vmax","Vmin");
-printf("%20.16g%20.16g%20.16g%20.16g%20.16g%20.16g\n",params.kappa,params.a,max,min,Vmax,Vmin);
-printf("\n");
+//Filename fo = "data/nr/3dpot/extrema_Kappa_"+nts(params.kappa)+"_a_"+nts(params.a)+".dat";
 
 return 0;
 }
