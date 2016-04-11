e811404ab0aec54e79937aefe278e06c1f7a6609
og113 <og113@imperial.ac.uk>
2016-04-11 16:14:30 +0100

    added a pic of V3d from mathematica, no legend unfortunately

--- csrc/3dpotential.cc
+++ csrc/3dpotential.cc
@@ -32,21 +32,54 @@ void params_for_V::setFromParameters(const Parameters& p) {
 	N.B. defined to work with gsl
 -------------------------------------------------------------------------------------------------------------------------*/
 
-double V (double r, void * parameters) {
+double V0 (double r, void * parameters) {
+	struct params_for_V* params = (struct params_for_V *)parameters;
+	double a = params->a;
+	double kappa = params->kappa;
+	return 2.0 - PI/a - r - kappa/(4.0*PI*sqrt(pow(a,2) + pow(r,2)));
+}
+
+double dV0 (double r, void * parameters) {
+	struct params_for_V* params = (struct params_for_V *)parameters;
+	double a = params->a;
+	double kappa = params->kappa;
+	return - 1.0 + (r*kappa)/(4.0*PI*pow(pow(a,2) + pow(r,2),1.5));
+}
+
+double ddV0 (double r, void * parameters) {
+	struct params_for_V* params = (struct params_for_V *)parameters;
+	double a = params->a;
+	double kappa = params->kappa;
+	return ((pow(a,2) - 2.0*pow(r,2))*kappa)/(4.0*PI*pow(pow(a,2) + pow(r,2),2.5));
+}
+
+double mV0 (double r, void * parameters) {
+	return -V0(r, parameters);
+}
+
+double mdV0 (double r, void * parameters) {
+	return -dV0(r, parameters);
+}
+
+double mddV0 (double r, void * parameters) {
+	return -ddV0(r, parameters);
+}
+
+double V4 (double r, void * parameters) {
 	struct params_for_V* params = (struct params_for_V *)parameters;
 	double a = params->a;
 	double kappa = params->kappa;
 	return 2.0 - r - ((-(1.0/(a*exp(pow(r,2)/pow(a,2)))) + 1.0/sqrt(pow(a,2) + pow(r,2)))*kappa)/(4.0*PI);
 }
 
-double dV (double r, void * parameters) {
+double dV4 (double r, void * parameters) {
 	struct params_for_V* params = (struct params_for_V *)parameters;
 	double a = params->a;
 	double kappa = params->kappa;
 	return  - 1.0 - (((2.0*r)/(pow(a,3)*exp(pow(r,2)/pow(a,2))) - r/pow(pow(a,2) + pow(r,2),1.5))*kappa)/(4.0*PI);
 }
 
-double ddV (double r, void * parameters) {
+double ddV4 (double r, void * parameters) {
 	struct params_for_V* params = (struct params_for_V *)parameters;
 	double a = params->a;
 	double kappa = params->kappa;
@@ -56,15 +89,15 @@ double ddV (double r, void * parameters) {
 			 + pow(r,2),2.5))*kappa)/(4.0*PI);
 }
 
-double mV (double y, void * parameters) {
-	return -V(y, parameters);
+double mV4 (double y, void * parameters) {
+	return -V4(y, parameters);
 }
 
-double mdV (double y, void * parameters) {
-	return -dV(y, parameters);
+double mdV4 (double y, void * parameters) {
+	return -dV4(y, parameters);
 }
 
-double mddV (double y, void * parameters) {
-	return -ddV(y, parameters);
+double mddV4 (double y, void * parameters) {
+	return -ddV4(y, parameters);
 }
 
--- include/3dpotential.h
+++ include/3dpotential.h
@@ -37,16 +37,28 @@ struct params_for_V {
 	N.B. defined to work with gsl
 -------------------------------------------------------------------------------------------------------------------------*/
 
-double V (double y, void * parameters);
+double V0 (double y, void * parameters);
 
-double dV (double y, void * parameters);
+double dV0 (double y, void * parameters);
 
-double ddV (double y, void * parameters);
+double ddV0 (double y, void * parameters);
 
-double mV (double y, void * parameters);
+double mV0 (double y, void * parameters);
 
-double mdV (double y, void * parameters);
+double mdV0 (double y, void * parameters);
 
-double mddV (double y, void * parameters);
+double mddV0 (double y, void * parameters);
+
+double V4 (double y, void * parameters);
+
+double dV4 (double y, void * parameters);
+
+double ddV4 (double y, void * parameters);
+
+double mV4 (double y, void * parameters);
+
+double mdV4 (double y, void * parameters);
+
+double mddV4 (double y, void * parameters);
 
 #endif // __3DPOTENTIAL_H_INCLUDED__
