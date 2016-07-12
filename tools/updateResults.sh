fb460b6913b366351aea3034a2ef3abd7f25222a
og113 <og113@imperial.ac.uk>
2016-07-12 15:52:31 +0100

    improved updateResults.sh

--- tools/updateResults.sh
+++ tools/updateResults.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 # update the sqlite database with new results
+## to do: should get it to take databaseFile and table as inputs (with the given defaults if not input)
 
 if [ -z "$1" ]
 then
 	echo "must supply input file to updateResults";
 	exit 1;
-elif [ -z "$2" ]
+elif [ "$#" -eq 1 ]
 then
-	databaseFile="db/nr/nr.sqlite"
-	table="nrtemp"
-	
-	resultsFile=$1;
-	tempResultsFile=$(echo "$1" | sed 's/results\/nr/temp/');
-	databaseResultsFile=$(echo "$1" | sed 's/results/db/');
-	deltaResultsFile=$(echo "$databaseResultsFile" | sed 's/\([^\/]\+\)$/delta_\1/');
-	
-	echo "input     : $resultsFile";
-	echo "updating  : $databaseResultsFile";
-	echo "          : $deltaResultsFile";
-	echo "          : $databaseFile";
-	
+	resultsFile=$1;	
+	table="nrtemp";
+	databaseFile="db/nr/nr.sqlite";
+elif [ "$#" -eq 2 ]
+then
+	resultsFile=$1;	
+	table=$2;
+	databaseFile="db/nr/nr.sqlite";
+elif [ "$#" -eq 3 ]
+then
+	resultsFile=$1;	
+	table=$2;
+	databaseFile=$3;
 else
 	echo "too many arguments for updateResults";
 	exit 1;
 fi
+
+# filenames generated from input
+tempResultsFile=$(echo "$resultsFile" | sed 's/results\/nr/temp/');
+databaseResultsFile=$(echo "$resultsFile" | sed 's/results/db/');
+deltaResultsFile=$(echo "$databaseResultsFile" | sed 's/\([^\/]\+\)$/delta_\1/');
+
+echo "input     : $resultsFile";
+echo "updating  : $databaseResultsFile";
+echo "          : $deltaResultsFile";
+echo "          : $databaseFile";
 
 # copying results into temp file
 cat $resultsFile > $tempResultsFile
