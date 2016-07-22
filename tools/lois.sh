31c6b6f222014215fd5411ca8fa48d757ffb9354
Oli Gould <og113@imperial.ac.uk>
2016-07-22 20:52:50 +0100

    added tools/lois.sh

---
+++ tools/lois.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# a quick and dirty bash program to increase "B" and submit a job "subs/nrmain0.sub" with the appropriate arguments
+
+# defining some files
+resultsFile="results/nr/nr4.csv"
+inputsFile="nrinputs/highTemp/inputs0"
+#inputsFile=inputs4
+jobFile="subs/nrmain0.sub"
+
+echo "results file = $resultsFile"
+echo "inputs file = $inputsFile"
+echo "job file = $jobFile"
+
+# finding the parameter "B" on the last line of the results file, after the 4th comma and before the 5th
+Bstart=$(tail -n 1 "$resultsFile" | sed -n "s/^\([0-9.-]\+\),\([0-9.-]\+\),\([0-9.-]\+\),\([0-9.-]\+\),\([0-9.-]\+\),.*/\5/p");
+
+# echoing Bstart
+echo "Bstart = $Bstart"
+
+# defining Bend as 10% higher
+#Bend=$(bc <<< "scale=6;$Bstart*1.1");
+Bend=$(python -c "print $Bstart*1.1");
+
+# echoing B_end
+echo "Bend = $Bend"
+
+# writing B_start and B_end to inputs file
+sed -i "s/^B\([^0-9.-]\+\)\([0-9.-]\+\) /B\1$Bstart/" $inputsFile
+sed -i "s/^B\([^0-9.-]\+\)\([0-9.-]\+\)\([^0-9.-]\+\)\([0-9.-]\+\) /B\1\2\3$Bend/" $inputsFile
+
+# submitting job
+echo "submitting job"
+msub $jobfile
