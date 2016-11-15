4aec46f5a5f6532cf138dde3ab2eaf214649831b
Oliver Gould <dc-goul1@cosmos.private.damtp.cam.ac.uk>
2016-11-15 08:56:49 +0000

    added early results for pot=15

--- tools/mergeResults.sh
+++ tools/mergeResults.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-sed -i '/<<</d' results/nr/nr6error.csv
-sed -i '/>>>/d' results/nr/nr6error.csv
-sed -i '/===/d' results/nr/nr6error.csv
-sed -i '/<<</d' results/nr/nr6.csv
-sed -i '/>>>/d' results/nr/nr6.csv
-sed -i '/===/d' results/nr/nr6.csv
+results="results/nr/nr6.csv results/nr/nr6error.csv"
+
+echo "removing lines with <<<, >>> and === from:"
+
+for f in $results;
+do
+	echo $f
+	sed -i '/<<</d' $f
+	sed -i '/>>>/d' $f
+	sed -i '/===/d' $f
+done
