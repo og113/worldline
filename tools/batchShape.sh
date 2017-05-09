3821fa947a0a7c0f478520328ca9e26d68201fd3
Oli Gould <og113@imperial.ac.uk>
2017-05-09 10:41:57 +0100

    updated batchShape on laptop

--- tools/batchShape.sh
+++ tools/batchShape.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # program to do a couple of batches of ./shape and ./nrmain runs
 
+if [ $# -ne 1 ]; then
+    INPUTS="nrinputs/inputs0";
+else
+	INPUTS=$1;
+fi
+
 for i in {1..20};
 	do
-	INPUTS="nrinputs/inputs0";
-	Epsi=$(python -c "print 0.005*${i}");
-	sed -i "s/Epsi\([[:space:]]\+\)\[ \([0-9.]\+\)/Epsi                [ ${Epsi}/" $INPUTS;
+#	Epsi=$(python -c "print 0.005*${i}");
+	Epsi=$(bc <<< "scale=3;0.005*${i}");
+	echo "a = 0${Epsi}";
+	sed -i "s/Epsi\([[:space:]]\+\)\[ \([0-9.]\+\)/Epsi                [ 0${Epsi}/" $INPUTS;
 	./shape -inputs $INPUTS -shape straightDisjoint -higherOrder 1;
-	./nrmain -inputs $INPUTS -pot thermalDisjoint -pass 1 -eigen 1 >> temp/shiftResults.txt;
+	./nrmain -inputs $INPUTS -guess 1 -step 0 -old 0 -pot thermalDisjointLR -gaussian 1 -gaussianLR 1 -mu_a 0 -nonrel 0 -allTests 0 -fixdz 1 -fixtlr 1 -fixdislr 1 -print none -auto_a 0 -conservation 0 -redo 1 -redoErrors 0 -verbose 1 -pass 1 -eigen 1
 done;
